c5ecd035c4a1a1d28e3ce13c885175497cd17f31
Hunter Leath <h@hunterleath.com>
2015-10-17 01:20:15 -0400

    frontend/js/profile/controller.js

--- frontend/js/profile/controller.js
+++ frontend/js/profile/controller.js
@@ -5,7 +5,9 @@ var plusProfileControllers = angular.module('plus.profile', []);
 plusProfileControllers.controller('profileCtrl', function($scope, api) {
     $scope.status = {
         none: "Prefer Not to Disclouse",
-        negative: "HIV Negative"
+        negative: "HIV Negative",
+        positive: "HIV Postivie",
+        aids: "AIDS",
     }
     
     api.profile.current().then(function(profile) {
