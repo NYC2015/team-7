57c23ab368b4027ec21808ccc9a3d4a79dc5cace
Hunter Leath <h@hunterleath.com>
2015-10-17 02:58:43 -0400

    frontend/js/register/controller.js

--- frontend/js/register/controller.js
+++ frontend/js/register/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var registerCtrls = angular.module('plus.registration', []);
+var registerCtrls = angular.module('plus.registration', ['plus.api']);
 
 registerCtrls.controller('registerCtrl', function($scope) {
 });
