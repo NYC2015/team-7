15cddc515fafdcf2276415875b40d75f480ed6b4
Hunter Leath <h@hunterleath.com>
2015-10-17 02:59:38 -0400

    frontend/js/register/controller.js

--- frontend/js/register/controller.js
+++ frontend/js/register/controller.js
@@ -4,6 +4,8 @@ var registerCtrls = angular.module('plus.registration', ['plus.api']);
 
 registerCtrls.controller('registerCtrl', function($scope, api) {
     $scope.register = function() {
-        
+        api.login.register({
+            name: $scope.username,
+        })
     }
 });
