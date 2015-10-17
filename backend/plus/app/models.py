c1a931a00e7fbe80920749afe87871b1d464be4d
Dylan Ferrer <dlf3x@virginia.edu>
2015-10-16 21:33:59 -0400

    Updated models

--- backend/plus/app/models.py
+++ backend/plus/app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Profile(models.Model):
-	id = models.AutoField(primary_key=True)
+class User(models.Model):
 	is_anonymous = models.BooleanField(default=True)
 	DISEASE_CHOICES = (
 		(0, 'Survivor'),
@@ -12,3 +11,11 @@ class Profile(models.Model):
 	diseases = models.IntegerField(max_length = 1, choices = DISEASE_CHOICES)
 	name = models.CharField(max_length = 100, blank = True)
 	pseudonym = models.CharField(max_length = 100, blank = True)
+
+class Chat(models.Model):
+	users = models.ManyToManyField(User, 'the chat participants')
+
+class Message(models.Model):
+	chat = models.ForeignKey(Chat)
+	sender = models.ForeignKey(User)
+	content = models.TextField()
