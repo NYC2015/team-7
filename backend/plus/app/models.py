648ab3e3bfc11859b63ab5ddd82e501a9cbe01e1
Dylan Ferrer <dlf3x@virginia.edu>
2015-10-16 22:00:53 -0400

    Starting on views

--- backend/plus/app/models.py
+++ backend/plus/app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
+class Profile(models.Model):
+	user = models.OneToOneField(User)
 	is_anonymous = models.BooleanField(default=True)
 	DISEASE_CHOICES = (
 		(0, 'Survivor'),
@@ -12,6 +14,7 @@ class User(models.Model):
 	diseases = models.IntegerField(max_length = 1, choices = DISEASE_CHOICES)
 	name = models.CharField(max_length = 100, blank = True)
 	pseudonym = models.CharField(max_length = 100, blank = True)
+	current_phone_number = models.CharField(max_length = 12)
 
 class Chat(models.Model):
 	users = models.ManyToManyField(User, 'the chat participants')
--- backend/plus/app/views.py
+++ backend/plus/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from models import *
 
 # Create your views here.
+def community(request):
+	return JsonResponse( {'community' : Community.objects.all() })
