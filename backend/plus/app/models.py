96820f9bb673825a263249a2820a1324c07ec493
Dylan Ferrer <dlf3x@virginia.edu>
2015-10-16 21:21:57 -0400

    MOAR COMMITS

--- backend/plus/app/models.py
+++ backend/plus/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+	id = models.AutoField(primary_key=True)
+	is_anonymous = models.BooleanField(default=True)
+	DISEASE_CHOICES = (
+		(0, 'Survivor'),
+		(1, 'HIV'),
+		(2, 'AIDS')
+	)
+	diseases = models.IntegerField(max_length = 1, choices = DISEASE_CHOICES)
+	name = models.CharField(max_length = 100, blank = True)
+	pseudonym = models.CharField(max_length = 100, blank = True)
