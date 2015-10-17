e2e2eef860c6adceb64812db7bb5d7363453d991
Brent Baumgartner <brent@brentbaum.com>
2015-10-16 22:26:08 -0400

    Api fix + gitignore sasscache

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -2,30 +2,16 @@
 
 var plusApi = angular.module('plus.api', []);
 
-var fakeAPICall = function(response, args) {
-    return function() {
-        var defer = $q.defer();
-
-        setTimeout(function() {
-            defer.resolve(response);
-        }, 15);
-
-        return defer.promise;
-    }
-}
-
 var conversation = {
     id: 10,
     name: undefined,
     participants: [profile, profile],
-    messages: [
-        {
-            from: 10,
-            time: "5 seconds ago",
-            body: "Hello, world",
-        }
-    ]
-}
+    messages: [{
+        from: 10,
+        time: "5 seconds ago",
+        body: "Hello, world"
+    }]
+};
 
 var profile = {
     id: 10,
@@ -34,10 +20,10 @@ var profile = {
     privacy: {
         none: true,
         hiv: true,
-        aids: false,
+        aids: false
     },
     status: "hiv"
-}
+};
 
 var post = {
     title: "free screenings + counseling at 123 roadd",
@@ -45,47 +31,56 @@ var post = {
     poster: profile,
     votes: 50,
     comments: [comment, comment, comment]
-}
+};
 
 var comment = {
     body: "blah blah blah, comment",
     poster: profile
-}
+};
 
 var resource = {
     title: "hello, world",
     body: "howdy",
     locality: 2,
     from: "doctor kwon"
-}
+};
 
-plsApi.factory('api', function($q) {
+plusApi.factory('api', function($q) {
+    var fakeAPICall = function(response, args) {
+        return function() {
+            var defer = $q.defer();
+
+            setTimeout(function() {
+                defer.resolve(response);
+            }, 15);
+
+            return defer.promise;
+        };
+    };
+
     return {
         profile: {
             current: fakeAPICall(profile)
         },
         chat: {
             sendMessage: fakeAPICall(
-                true,
-                ["conversationId", "message"]
+                true, ["conversationId", "message"]
             ),
-            all: fakeAPICall({
-                [conversation, conversation]
-            })
+            all: fakeAPICall(
+                true, [conversation, conversation]
+            )
         },
         community: {
             postComment: fakeAPICall(
-                true,
-                ["postId", "message"]
+                true, ["postId", "message"]
             ),
             postStory: fakeAPICall(
-                true,
-                ["title", "body"]
+                true, ["title", "body"]
             ),
             all: fakeAPICall([post, post])
         },
         learn: {
             all: fakeAPICall([resource, resource])
         }
-    }
-})
+    };
+});
