f1893d4993ad46aebdabcd1621f55a899a77a7b8
Brent Baumgartner <brent@brentbaum.com>
2015-10-17 03:05:03 -0400

    App states

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,29 +3,30 @@ angular.module("plus", ["ui.router",
     "plus.chat",
     "plus.learn",
     "plus.profile",
-    "plus.header"
+    "plus.header",
+    "ui.bootstrap"
 ]).config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/community");
 
     $stateProvider
-    .state('community', {
-        url: "/community",
-        controller: "communityCtrl",
-        templateUrl: "js/community/community.html"
-    })
-    .state('chat', {
-        url: "/chat",
-        controller: "chatCtrl",
-        templateUrl: "js/chat/chat.html"
-    })
-    .state('learn', {
-        url: "/learn",
-        controller: "learnCtrl",
-        templateUrl: "js/learn/learn.html"
-    })
-    .state('profile', {
-        url: "/profile",
-        controller: "profileCtrl",
-        templateUrl: "js/profile/profile.html"
-    });
+        .state('community', {
+            url: "/community",
+            controller: "communityCtrl",
+            templateUrl: "js/community/community.html"
+        })
+        .state('chat', {
+            url: "/chat",
+            controller: "chatCtrl",
+            templateUrl: "js/chat/chat.html"
+        })
+        .state('learn', {
+            url: "/learn",
+            controller: "learnCtrl",
+            templateUrl: "js/learn/learn.html"
+        })
+        .state('profile', {
+            url: "/profile",
+            controller: "profileCtrl",
+            templateUrl: "js/profile/profile.html"
+        });
 });
