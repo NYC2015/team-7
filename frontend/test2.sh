029496a6b1543e3978803bb2f51eba4d51cdd9de
Dylan Ferrer <dlf3x@virginia.edu>
2015-10-17 03:10:09 -0400

    updates

--- frontend/test2.sh
+++ frontend/test2.sh
@@ -1,9 +1,8 @@
 for (( ;; ))
     do
-        echo "1" >> test2.txt
+        echo "2" >> test2.txt
         git add -A
         git commit -m "updates"
-        git pull
+        git pull -Xtheirs
         git push
-        sleep 1
     done
