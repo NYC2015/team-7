5db31f27c80640f384bac840b51bc57613374a9c
Hunter Leath <h@hunterleath.com>
2015-10-17 03:00:13 -0400

    frontend/js/register/controller.js

--- frontend/js/register/controller.js
+++ frontend/js/register/controller.js
@@ -8,7 +8,7 @@ registerCtrls.controller('registerCtrl', function($scope, api) {
             username: $scope.username,
             password: $scope.password,
             'phone_number': $scope.number,
-            disease: $scope
+            disease: $scope.status,
         })
     }
 });
