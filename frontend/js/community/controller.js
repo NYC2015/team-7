e4f288e02a3368dfe6de36f0674cc79e098793dc
Brent Baumgartner <brent@brentbaum.com>
2015-10-16 23:09:15 -0400

    Community stuff!

--- frontend/js/community/controller.js
+++ frontend/js/community/controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
-var plusCommunityControllers = angular.module('plus.community', []);
+var plusCommunityControllers = angular.module('plus.community', ['plus.api']);
 
-plusCommunityControllers.controller('communityCtrl', function($scope, api) {
-    api.profile.current().success(function(profile) {
-        $scope.profile = profile;
+plusCommunityControllers.controller('communityCtrl', function($scope, communityService) {
+    $scope.community = communityService.exports;
+    communityService.ready.promise.then(function() {
+        // do setup;
     });
+
+    $scope.postStory = communityService.postStory;
 });
