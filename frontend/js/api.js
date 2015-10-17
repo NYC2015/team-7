3acb8ac91ab33bf98655dce5e224353fb68fee95
Hunter Leath <h@hunterleath.com>
2015-10-16 21:53:59 -0400

    frontend/js/api.js

---
+++ frontend/js/api.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var plusApi = angular.module('plus.api');
+
+plsApi.factory('plusAPI', function() {
+    return {
+        profile: {
+            current: function() {
+                return {
+                    name: "Hunter Leath",
+                    picture: "http://google.com",
+                    privacy: {
+                        none: true,
+                        hiv: true,
+                        aids: false,
+                    }
+                }
+            }
+        }
+    }
+})
