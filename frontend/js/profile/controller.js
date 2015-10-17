ff5d315eb578a311f8f50ba742dfa4c84b0223d5
Hunter Leath <h@hunterleath.com>
2015-10-17 01:21:44 -0400

    frontend/js/profile/controller.js

--- frontend/js/profile/controller.js
+++ frontend/js/profile/controller.js
@@ -11,7 +11,9 @@ plusProfileControllers.controller('profileCtrl', function($scope, api) {
     };
 
     $scope.privacy = {
-        
+        none: "Anonymous to All Users",
+        positive: "Known to HIV Positive Users",
+        negative: "Known to All Users",
     }
     
     api.profile.current().then(function(profile) {
