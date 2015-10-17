ae5280004fe397924e9f7e739b1afdd250cbc02b
Hunter Leath <h@hunterleath.com>
2015-10-17 02:13:46 -0400

    frontend/js/login/controller.js

--- frontend/js/login/controller.js
+++ frontend/js/login/controller.js
@@ -4,6 +4,12 @@ var loginCtrls = angular.module('plus.login', []);
 
 loginCtrls.controller('loginCtrl', function($scope) {
     $scope.login = function() {
+        if ($scope.username == "" || $scope.username === undefined) {
+            return;
+        }
         
+        if ($scope.password == "" || $scope.password === undefined) {
+            return;
+        }
     }
 });
